bf897ca4796632b94b493a4b3abe3d76667a6a47
Caiuno01 <43186403+Caiuno01@users.noreply.github.com>
2018-09-24 18:51:00 -0300

    Construtor PesoComponente

--- workspace/Atento/src/com/atento/entidade/PesoComponente.java
+++ workspace/Atento/src/com/atento/entidade/PesoComponente.java
@@ -16,4 +16,72 @@ public class PesoComponente {
 
 	private int youtube;
 
+	public PesoComponente(int facebook, int nAmigos, int twitter, int nSeguidores, int linkdin, int nConexoes,
+			int youtube) {
+		super();
+		this.facebook = facebook;
+		this.nAmigos = nAmigos;
+		this.twitter = twitter;
+		this.nSeguidores = nSeguidores;
+		this.linkdin = linkdin;
+		this.nConexoes = nConexoes;
+		this.youtube = youtube;
+	}
+
+	public int getFacebook() {
+		return facebook;
+	}
+
+	public void setFacebook(int facebook) {
+		this.facebook = facebook;
+	}
+
+	public int getnAmigos() {
+		return nAmigos;
+	}
+
+	public void setnAmigos(int nAmigos) {
+		this.nAmigos = nAmigos;
+	}
+
+	public int getTwitter() {
+		return twitter;
+	}
+
+	public void setTwitter(int twitter) {
+		this.twitter = twitter;
+	}
+
+	public int getnSeguidores() {
+		return nSeguidores;
+	}
+
+	public void setnSeguidores(int nSeguidores) {
+		this.nSeguidores = nSeguidores;
+	}
+
+	public int getLinkdin() {
+		return linkdin;
+	}
+
+	public void setLinkdin(int linkdin) {
+		this.linkdin = linkdin;
+	}
+
+	public int getnConexoes() {
+		return nConexoes;
+	}
+
+	public void setnConexoes(int nConexoes) {
+		this.nConexoes = nConexoes;
+	}
+
+	public int getYoutube() {
+		return youtube;
+	}
+
+	public void setYoutube(int youtube) {
+		this.youtube = youtube;
+	}
+	
 }
