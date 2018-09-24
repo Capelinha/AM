851d7c52622a6e2bf68b9c08389705cb0b230655
Caiuno01 <43186403+Caiuno01@users.noreply.github.com>
2018-09-24 18:56:27 -0300

    Construtor PesoTag

--- workspace/Atento/src/com/atento/entidade/PesoTag.java
+++ workspace/Atento/src/com/atento/entidade/PesoTag.java
@@ -6,4 +6,25 @@ public class PesoTag {
 
 	private Tag tag;
 
+	public PesoTag(int peso, Tag tag) {
+		super();
+		this.peso = peso;
+		this.tag = tag;
+	}
+
+	public int getPeso() {
+		return peso;
+	}
+
+	public void setPeso(int peso) {
+		this.peso = peso;
+	}
+
+	public Tag getTag() {
+		return tag;
+	}
+
+	public void setTag(Tag tag) {
+		this.tag = tag;
+	}
 }
