69904ad22294a94f3ab64041accdeb773788c817
Mateus Igreja <mateus@nuplex.com.br>
2018-10-08 16:04:46 -0300

    UPDATE: Mudança no somar logins

--- workspace/Atento/src/com/atento/conexao/Conexao.java
+++ workspace/Atento/src/com/atento/conexao/Conexao.java
@@ -7,9 +7,9 @@ import java.sql.SQLException;
 public class Conexao {
 	private static Connection connection;
     private static final String DRIVER = "oracle.jdbc.driver.OracleDriver";
-    private static final String URL = "jdbc:oracle:thin:@localhost:1521:xe";
-    private static final String USUARIO = "am";
-    private static final String SENHA = "am";
+    private static final String URL = "jdbc:oracle:thin:@oracle.fiap.com.br:1521:orcl";
+    private static final String USUARIO = "rm77807";
+    private static final String SENHA = "L@reno1501";
 
     public Conexao() {}
 
